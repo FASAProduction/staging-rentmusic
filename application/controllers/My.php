8fe23477a6cfff15d33f519917963448ba52a8fe
fprofficial <fasaproductionofficial@gmail.com>
2021-07-29 09:18:45 +0700

    beberapa

--- application/controllers/My.php
+++ application/controllers/My.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 date_default_timezone_set('Asia/Jakarta');
 
-class Dashboard extends CI_Controller {
+class My extends CI_Controller {
 	
 	function __construct(){
         parent::__construct();
