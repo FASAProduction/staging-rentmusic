10ab9647285c3f73673fab4720ad4fb2d24d1980
fprofficial <fasaproductionofficial@gmail.com>
2021-08-05 10:21:04 +0700

    Feat - Customize UI #1

---
+++ application/views/menu/bottom_js.php
@@ -0,0 +1,13 @@
+<!-- Javascript files-->
+<script src="<?php echo base_url(); ?>/komponen/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>/komponen/vendor/popper.js/umd/popper.min.js"> </script>
+    <script src="<?php echo base_url(); ?>/komponen/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>/komponen/vendor/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="<?php echo base_url(); ?>/komponen/vendor/waypoints/lib/jquery.waypoints.min.js"> </script>
+    <script src="<?php echo base_url(); ?>/komponen/vendor/jquery.counterup/jquery.counterup.min.js"> </script>
+    <script src="<?php echo base_url(); ?>/komponen/vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="<?php echo base_url(); ?>/komponen/vendor/owl.carousel2.thumbs/owl.carousel2.thumbs.min.js"></script>
+    <script src="<?php echo base_url(); ?>/komponen/js/jquery.parallax-1.1.3.js"></script>
+    <script src="<?php echo base_url(); ?>/komponen/vendor/bootstrap-select/js/bootstrap-select.min.js"></script>
+    <script src="<?php echo base_url(); ?>/komponen/vendor/jquery.scrollto/jquery.scrollTo.min.js"></script>
+    <script src="<?php echo base_url(); ?>/komponen/js/front.js"></script>
